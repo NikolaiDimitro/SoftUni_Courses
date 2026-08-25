457d07025f79a29da8bfdff7b100dc79364affd5
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-25 19:07:46 +0300

    Vacation Vook List

---
+++ JS Basic/02. First Steps In Coding - Exercise/04. Vacation Books List.js
@@ -0,0 +1,7 @@
+function vacationBooksList(countPages, pagesForHour, countDays) {
+
+    console.log(countPages / pagesForHour / countDays)
+
+}
+vacationBooksList(212, 20, 2);
+vacationBooksList(432, 15, 4);
