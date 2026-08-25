460be731f487263337ff621705d4b4d47a397217
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-25 15:03:04 +0300

    Project Creation

---
+++ First Steps In Coding - Lab/07. Projects Creation.js
@@ -0,0 +1,7 @@
+function projectCreation(name, countProjects) {
+
+    console.log(`The architect ${name} will need ${countProjects * 3} hours to complete ${countProjects} project/s.`)
+
+}
+projectCreation("George", 4);
+projectCreation("Sanya", 9);
