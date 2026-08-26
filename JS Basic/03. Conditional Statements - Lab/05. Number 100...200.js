dc73775118eba48b64732c3b4467779bd4eb5c33
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-26 22:01:59 +0300

    Number 100 to 200

---
+++ JS Basic/03. Conditional Statements - Lab/05. Number 100...200.js
@@ -0,0 +1,19 @@
+function numbers100To200(speed) {
+
+    if (speed < 100) {
+
+        console.log('Less than 100');
+
+    } else if (speed >= 100 && speed <= 200) {
+
+        console.log('Between 100 and 200');
+
+    } else {
+
+        console.log('Greater than 200');
+
+    }
+}
+numbers100To200(95);
+numbers100To200(120);
+numbers100To200(210);
