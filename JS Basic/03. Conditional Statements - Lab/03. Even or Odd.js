652e5f7a20629cb7e0656f2a2e6d633e15d8fdbf
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-26 18:19:59 +0300

    Even or Odd

---
+++ JS Basic/03. Conditional Statements - Lab/03. Even or Odd.js
@@ -0,0 +1,16 @@
+function evenOrOdd(numb) {
+
+    if (numb % 2 === 0) {
+
+        console.log('even');
+
+    } else {
+
+        console.log('odd');
+
+    }
+}
+evenOrOdd(2);
+evenOrOdd(3);
+evenOrOdd(25);
+evenOrOdd(1024);
