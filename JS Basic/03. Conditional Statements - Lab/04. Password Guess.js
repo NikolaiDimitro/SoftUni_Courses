6b9c7ae25a4092ad9f555f3abdac3f46dc7cc810
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-26 19:25:04 +0300

    Password Guess

---
+++ JS Basic/03. Conditional Statements - Lab/04. Password Guess.js
@@ -0,0 +1,15 @@
+function passwordGuess(password) {
+
+    if (password === "s3cr3t!P@ssw0rd") {
+
+        console.log('Welcome');
+
+    } else {
+
+        console.log('Wrong password!');
+
+    }
+}
+passwordGuess("qwerty");
+passwordGuess("s3cr3t!P@ssw0rd");
+passwordGuess("s3cr3t!p@ss");
