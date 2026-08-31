92f599ee2f8d5e578517e7934fba14579451be66
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-31 20:30:17 +0300

    Number 1...N with Step 3

---
+++ JS Basic/05. For Loop - Lab/03. Numbers 1...N with Step 3.js
@@ -0,0 +1,11 @@
+function numbers1ToNWithStep3(number) {
+
+    for (let i = 1; i <= number; i += 3) {
+
+        console.log(i);
+
+    }
+}
+numbers1ToNWithStep3(10);
+numbers1ToNWithStep3(7);
+numbers1ToNWithStep3(15);
