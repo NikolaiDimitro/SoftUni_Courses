a2865c3cd9fc6b1de32c717a1263be0c8fa482bb
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-08-31 21:24:26 +0300

    Character Sequence

---
+++ JS Basic/05. For Loop - Lab/05. Character Sequence.js
@@ -0,0 +1,11 @@
+function characterSequence(word) {
+
+    for (let i = 0; i <= word.length - 1; i++) {
+
+        let letter = word[i];
+        console.log(letter);
+
+    }
+}
+characterSequence("softuni");
+characterSequence("ice cream");
