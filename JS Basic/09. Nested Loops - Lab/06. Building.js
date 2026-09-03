1547c633017b793e23f5ce0f793f215426a17ef1
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-09-03 19:13:26 +0300

    Building

---
+++ JS Basic/09. Nested Loops - Lab/06. Building.js
@@ -0,0 +1,48 @@
+function building(countFloors, countRooms) {
+
+    for (let f = countFloors; f >= 1; f--) {
+
+        let str = '';
+
+
+
+        for (let r = 0; r < countRooms; r++) {
+
+
+
+
+            if (f === countFloors) {
+
+                str += `L${f}${r} `;
+
+
+            }
+
+            if (f % 2 === 1 && f !== countFloors) {
+
+                str += `A${f}${r} `;
+
+            } else if (f % 2 === 0 && f !== countFloors) {
+
+                str += `O${f}${r} `;
+
+            }
+
+
+        }
+
+        console.log(str);
+
+
+
+    }
+
+}
+// building(6,
+
+//     4);
+building(9,
+
+    5);
+// building();
+// building();
