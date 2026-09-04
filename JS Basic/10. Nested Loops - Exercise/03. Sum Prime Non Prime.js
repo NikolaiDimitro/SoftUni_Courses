0ee6b4aae3d21769004a9ad1418dc4c47973b573
Nikolai <nikolaidimitrov9012@gmail.com>
2026-09-04 12:51:13 +0300

    Sum Prime Non Prime

--- JS Basic/10. Nested Loops - Exercise/03. Sum Prime Non Prime.js
+++ JS Basic/10. Nested Loops - Exercise/03. Sum Prime Non Prime.js
@@ -2,79 +2,48 @@ function sumPrimeNonPrime(arr) {
 
     let index = 0;
     let sumPrNumb = 0;
-    let sumNPrimeNumb = 0
+    let sumNPrimeNumb = 0;
 
     while (arr[index] !== 'stop') {
 
-        for (let i = 0; i <= arr.length - 2; i++) {
+        let num = Number(arr[index]);
 
-            let num = Number(arr[i]);
+        if (num < 0) {
 
-
-            if (num < 0) {
-
-                console.log('Number is negative.');
-                break;
-
-
-            }
-
-            if (num % 2 === 0 || num % 3 === 0 || num % 7 === 0 || num % 10 === 5 || num % 10 === 0) {
-
-                sumPrNumb += num;
-
-            } else {
-
-                sumNPrimeNumb += num;
-
-            }
-
-
-
+            console.log('Number is negative.');
             index++;
-
+            continue;
         }
 
+        if (num === 0 || num === 1) {
 
+            sumNPrimeNumb += num;
+            index++;
+            continue;
+        }
 
+        let isPrime = true;
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            sumPrNumb += num;
+        } else {
+            sumNPrimeNumb += num;
+        }
+
+        index++;
     }
 
     console.log(`Sum of all prime numbers is: ${sumPrNumb}`);
     console.log(`Sum of all non prime numbers is: ${sumNPrimeNumb}`);
-
-
-
-
 }
-sumPrimeNonPrime(["3",
-
-    "9",
-
-    "0",
-
-    "7",
-
-    "19",
-
-    "4",
-
-    "stop"]);
-// sumPrimeNonPrime(["30",
-
-//     "83",
-
-//     "33",
-
-//     "-1",
-
-//     "20",
-
-//     "stop"]);
-// sumPrimeNonPrime(["0",
-
-//     "-9",
-
-//     "0",
-
-//     "stop"]);
-// sumPrimeNonPrime();
+sumPrimeNonPrime(["3", "9", "0", "7", "19", "4", "stop"]);
+sumPrimeNonPrime(["30", "83", "33", "-1", "20", "stop"]);
+sumPrimeNonPrime(["0", "-9", "0", "stop"]);
