c0d2da7b3382017a1e1c7ac7b0be0facf60bca56
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-09-05 21:16:50 +0300

    Celsius to Fahrenheit

---
+++ JS Basic/11. First Steps in Coding - More Exercises/03. Celsius to Fahrenheit.js
@@ -0,0 +1,9 @@
+function celsiusToFahrenheit(n) {
+
+console.log(((n * 1.8) + 32).toFixed(2));
+
+}
+celsiusToFahrenheit(25);
+celsiusToFahrenheit(0);
+celsiusToFahrenheit(-5.5);
+celsiusToFahrenheit(32.3);
