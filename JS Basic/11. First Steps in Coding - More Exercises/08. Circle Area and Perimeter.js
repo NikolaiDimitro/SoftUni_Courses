a9907dad7f4aaa814967e37403fcb2c1fb369e94
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-09-06 22:00:57 +0300

    Circle Area and Perimeter

---
+++ JS Basic/11. First Steps in Coding - More Exercises/08. Circle Area and Perimeter.js
@@ -0,0 +1,8 @@
+function circleAreaAndPerimeter(r) {
+
+    console.log((Math.PI * Math.pow(r, 2)).toFixed(2));
+    console.log((2 * Math.PI * r).toFixed(2));
+
+}
+circleAreaAndPerimeter(3);
+circleAreaAndPerimeter(4.5);
