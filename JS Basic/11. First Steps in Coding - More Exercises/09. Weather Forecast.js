83790dd3437f43574096aec2bb684f215b2ef745
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-09-06 22:07:20 +0300

    Weather Forecast

---
+++ JS Basic/11. First Steps in Coding - More Exercises/09. Weather Forecast.js
@@ -0,0 +1,15 @@
+function weatherForecast(str) {
+
+    if (str === 'sunny') {
+
+        console.log('It\'s warm outside!');
+
+    } else {
+
+        console.log('It\'s cold outside!');
+
+    }
+}
+weatherForecast('sunny');
+weatherForecast('cloudy');
+weatherForecast('snowy');
