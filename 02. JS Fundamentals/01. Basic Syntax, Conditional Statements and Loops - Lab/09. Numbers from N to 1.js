7d3390b5fb5638bd8fc8a39e94204b8bbd4aa4e6
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-09-27 00:33:02 +0300

    Numbers from N to 1

---
+++ 02. JS Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/09. Numbers from N to 1.js
@@ -0,0 +1,13 @@
+function numbersFromNTo1(n) {
+
+    let index = n;
+
+    while (index >= 1) {
+
+        console.log(index);
+        index--;
+
+    }
+}
+numbersFromNTo1(5);
+numbersFromNTo1(3);
